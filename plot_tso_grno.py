0e6e9a30b6719354815765dc9c5a4346d0a1a28c
Wilbert <wilbert.huibregtse@rws.nl>
2025-04-24 11:07:54 +0200

    Updated docstrings in plot scripts

--- plot_tso_grno.py
+++ plot_tso_grno.py
@@ -1,3 +1,20 @@
+"""
+For processing a new measurement, first run 'process_locations.py', and then 
+create a new data file using 'create_datafile.py'.
+
+If the function is called without a specified measurement date, the latest available
+measurement date will be used. To select a specific measurement date, you have two options:
+    - Specify the date as a string in 'YYYYMMDD' format, for example: measurement_date = '20240601'.
+    - Specify an integer, where:
+        - -1 corresponds to the latest measurement date,
+        - -2 corresponds to the second latest measurement date, and so on. For example: measurement_date = -5.
+
+If 'plot_mode' is set to 'multiple', by default the latest six measurements are used for plotting.
+However, you can adjust this by using the 'multiple_offset' parameter to select a different set of six consecutive measurements. For example:
+    - If 'multiple_offset = 2', the function will select six measurements starting from the third-most recent measurement.
+    - Similarly, 'multiple_offset = 7' will select the six measurements starting from the seventh-most recent, and so on.
+"""
+
 from plot_measurement import process_plots
 
 location = 'grno'
