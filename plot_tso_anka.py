44221b673a5e80a6ae9a3173684ff74f829aee4e
Wilbert <wilbert.huibregtse@rws.nl>
2025-05-13 11:25:47 +0200

    Processed several measurements

--- plot_tso_anka.py
+++ plot_tso_anka.py
@@ -12,7 +12,7 @@ measurement date will be used. To select a specific measurement date, you have t
 If 'plot_mode' is set to 'multiple', by default the latest six measurements are used for plotting.
 However, you can adjust this by using the 'multiple_offset' parameter to select a different set of six consecutive measurements. For example:
     - If 'multiple_offset = 2', the function will select six measurements starting from the third-most recent measurement.
-    - Similarly, 'multiple_offset = 7' will select the six measurements starting from the seventh-most recent, and so on.
+    - Similarly, 'multiple_offset = 7' will select the six measurements starting from the eight-most recent, and so on.
 """
 
 from plot_measurement import process_plots
--- plot_tso_veer.py
+++ plot_tso_veer.py
@@ -1,16 +1,18 @@
 """
-For processing a new measurement, first run 'rename_raw_tso_files.py' and then 
-create a new datafile with 'create_datafile.py'
+For processing a new measurement, first run 'process_locations.py', and then 
+create a new data file using 'create_datafile.py'.
 
-If the function is called without measurement date, the latest available
-measurement date is chosen, to chose a specific measurement date, 
-two options are available:
-    - Specify the date, for example : measurement_date = '20240601'
-    - Specify an integer, where -1 is the latest, -2 is the second latest, etc.,
-      example: measurement_date = -1
-    
-If plot_mode 'multiple' is used, the latest six measurements are used
+If the function is called without a specified measurement date, the latest available
+measurement date will be used. To select a specific measurement date, you have two options:
+    - Specify the date as a string in 'YYYYMMDD' format, for example: measurement_date = '20240601'.
+    - Specify an integer, where:
+        - -1 corresponds to the latest measurement date,
+        - -2 corresponds to the second latest measurement date, and so on. For example: measurement_date = -5.
 
+If 'plot_mode' is set to 'multiple', by default the latest six measurements are used for plotting.
+However, you can adjust this by using the 'multiple_offset' parameter to select a different set of six consecutive measurements. For example:
+    - If 'multiple_offset = 2', the function will select six measurements starting from the third-most recent measurement.
+    - Similarly, 'multiple_offset = 7' will select the six measurements starting from the eight-most recent, and so on.
 """
 
 from plot_measurement import process_plots
